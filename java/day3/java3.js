9867f4d3bd04e9d995d90c1a2bb4d2f0ff8eb824
quanghieu265 <hieu26594@gmail.com>
2021-03-18 16:51:29 +0700

    update bai tap buoi 3 java

--- java/day3/java3.js
+++ java/day3/java3.js
@@ -79,14 +79,14 @@ findMin([31, 6, 8, 4, 109, 5, 6, 77, 2, 99, 65]);
 
 //3. Tìm số lớn thứ 2 trong mảng số nguyên
 function findMax2(array) {
-    let max1 = array[0];    
-    let max2 = array[0];                              
+    let max1 = array[0];
+    let max2 = array[0];
     let a = array.length;
-    for (counter = 0; counter < a; counter++) {    
-        if (array[counter] > max1) {                
+    for (counter = 0; counter < a; counter++) {
+        if (array[counter] > max1) {
             max1 = array[counter];
         }
-        if(max1>array[counter] && array[counter]>max2){
+        if (max1 > array[counter] && array[counter] > max2) {
             max2 = array[counter];
         }
     }
@@ -96,14 +96,14 @@ findMax2([31, 6, 8, 4, 109, 5, 6, 77, 2, 99, 65]);
 
 //4. Trả về Vị trí đầu tiên của số lẻ, vị trí cuối cùng của số chẵn trong mảng bất kì.
 
-function soLeChan(array){
-    let mangLeChan=[];
+function soLeChan(array) {
+    let mangLeChan = [];
     let a = array.length;
     for (counter = 0; counter < a; counter++) {
-        if (array[counter] % 2 ===0 ) {                
+        if (array[counter] % 2 === 0) {
             mangLeChan.push(array[counter]);
         }
-        if (array[counter] % 2 !==0 ) {                
+        if (array[counter] % 2 !== 0) {
             mangLeChan.unshift(array[counter]);
         }
     }
@@ -111,3 +111,53 @@ function soLeChan(array){
 }
 console.log(soLeChan([31, 6, 8, 4, 109, 5, 6, 77, 2, 99, 65]));
 
+// Tra ve vi tri cuoi cung cua so chan trong mang
+
+function findLastEven(arr1) {
+    for (let i = arr1.length - 1; i >= 0; i--) {
+        if (arr1[i] % 2 === 0) {
+            return `Index: ${i} - value: ${arr1[i]}`;
+        }
+    }
+}
+
+let arr1 = [-4, 0, -3.6, -5, 8, 6, 9, 4, -6, 2, 7];
+
+
+console.log(findLastEven(arr1));
+
+//5. In một chuỗi cho trước theo thứ tự ngược lại (Ví dụ: Hello in thành olleH)
+
+function inChuoi(string1) {
+    let string2 = "";
+    for (let q = 0; q < string1.length; q++) {
+        string2 = string1[q] + string2;
+
+    }
+    console.log(string2);
+}
+
+inChuoi("xin chao cac ban");
+//6. Sắp xếp mảng số nguyên theo chiều tăng dần và giảm dần.
+
+function sapXepTang(arrNum) {
+    for (up = 0; up < arrNum.length; up++) {
+        if (arrNum[up] < 1) {       //Điều kiện là mảng số nguyên
+            console.log(`${arrNum} không phải là mảng các số nguyên`)
+            return;
+        }
+    }
+    for (n1 = 0; n1 < arrNum.length - 1; n1++) {    //vòng lặp cho số đầu tiên
+        for (n2 = n1 + 1; n2 < arrNum.length; n2++) {   // vòng lặp cho số thứ 2
+            let mid = [];                           // biến trung gian để thay đổi giá trị 2 số
+            if (arrNum[n1] > arrNum[n2]) {          // so sánh 2 số để đổi chỗ và giá trị cho nhau
+                mid = arrNum[n2];
+                arrNum[n2] = arrNum[n1];
+                arrNum[n1] = mid;
+            }
+        }
+    }
+    console.log(arrNum);
+}
+
+sapXepTang([9, 8, 1, 6, 7, 3, 9, 5, 4, 1]);
